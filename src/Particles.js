aff4e10a74b08db871fc19723345cf6340010e99
anastasiia <anastasiya1155@gmail.com>
2022-10-30 18:34:04 +0700

    make particles colourful and animate them

--- src/Particles.js
+++ src/Particles.js
@@ -1,27 +1,39 @@
 import { useControls } from "leva";
-import { useMemo } from "react";
-import { useLoader } from "@react-three/fiber";
+import { useMemo, useRef } from "react";
+import { useFrame, useLoader } from "@react-three/fiber";
 import {TextureLoader} from "three";
 import circleImg from "./textures/circle.png"
 
 function Particles() {
-  const {size, number, color} = useControls('particles', {
+  const ref = useRef();
+  const {size, number, color, multiColored} = useControls('particles', {
     size: 0.7,
     number: 5000,
-    color: '#304FFF'
+    color: '#304FFF',
+    multiColored: true,
   })
   const circleTexture = useLoader(TextureLoader, circleImg);
 
-  const positions = useMemo(() => {
-    const array = new Float32Array(number * 3);
+  const { positions, colors } = useMemo(() => {
+    const positions = new Float32Array(number * 3);
+    const colors = new Float32Array(number * 3);
     for (let i = 0; i < number; i++) {
-      array[i] = (Math.random() - 0.5) * 60;
+      positions[i] = (Math.random() - 0.5) * 60;
+      colors[i] = Math.random();
     }
-    return array;
+    return {positions, colors};
   }, [number]);
 
+  useFrame(({clock}) => {
+    const elapsedTime = clock.getElapsedTime()
+    if (ref.current) {
+      ref.current.position.z = (elapsedTime * 0.3) % 20
+    }
+  })
+
+  const props = multiColored ? {vertexColors: true} : {color}
   return (
-    <points>
+    <points ref={ref}>
       <bufferGeometry attach="geometry">
         <bufferAttribute
           attach="attributes-position"
@@ -29,12 +41,18 @@ function Particles() {
           count={positions.length / 3}
           itemSize={3}
         />
+        <bufferAttribute
+          attach="attributes-color"
+          array={colors}
+          count={colors.length / 3}
+          itemSize={3}
+        />
       </bufferGeometry>
       <pointsMaterial
         attach="material"
         size={size}
         sizeAttenuation
-        color={color}
+        {...props}
         alphaMap={circleTexture}
         transparent
         alphaTest={0.5}
